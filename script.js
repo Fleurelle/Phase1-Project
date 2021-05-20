e52a931196fcc737ffa9ff286f63acd0e37e133a
Rose <r.d.jeanlouis01@gmail.com>
2021-05-20 18:49:28 -0400

    Adding/Formatting Logo and form

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
+// For reset button to reload page
+function reloadP(){
+    location.reload()
+}
+//1. URL
+// const BASE_URL = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=27e8b185-779c-40f8-981e-8b70ee4c42b6'
 
-document.querySelector('body').appendChild(h2); 
+const BASE_URL = 'https://dictionaryapi.com/api/v3/references/collegiate/json/test?key=27e8b185-779c-40f8-981e-8b70ee4c42b6'
+
+//2. URL Request  
+fetch(BASE_URL)
+    .then(res => res.json())
+    .then(console.log)
+
+
+
+
+//3. Specify Header
+
+//4. Specify Body
